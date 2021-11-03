637e07b765230a72a7d660634bbf8365f3fb4c6e
Heath Stewart <heaths@microsoft.com>
2021-11-03 10:57:57 -0700

    Prepare for Conversations analysis

--- Program.cs
+++ Program.cs
@@ -2,14 +2,72 @@
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.Diagnostics.Tracing;
-using Azure.AI.Language.QuestionAnswering;
 using Azure;
+// using Azure.AI.Language.Conversations;
+using Azure.AI.Language.QuestionAnswering;
 using Azure.Core.Diagnostics;
 
-var command = new RootCommand
+var command = new RootCommand("Cognitive Service - Language sample application for testing new releases.")
 {
-    new Argument<Uri>(
-        "endpoint",
+    new Option<Uri>(
+        "--clu-endpoint",
+        getDefaultValue: () =>
+        {
+            if (Uri.TryCreate(Environment.GetEnvironmentVariable("CONVERSATIONS_URI"), UriKind.Absolute, out var endpoint))
+            {
+                return endpoint;
+            }
+
+            return null;
+        },
+        description: "Conversations (formerly QnA Maker) endpoint. The default is the CONVERSATIONS_URI environment variable, if set.")
+    {
+        IsRequired = true,
+    },
+
+    new Option<string>(
+        "--clu-key",
+        getDefaultValue: () =>
+        {
+            return Environment.GetEnvironmentVariable("QUESTIONANSWERING_PROJECT");
+        },
+        description: "Conversations API key. The default is the CONVERSATIONS_KEY environment variable, if set.")
+    {
+        IsRequired = true,
+    },
+
+    new Option<string>(
+        "--clu-project",
+        getDefaultValue: () =>
+        {
+            return Environment.GetEnvironmentVariable("CONVERSATIONS_PROJECT");
+        },
+        description: "Conversations project to query. The default is the CONVERSATIONS_PROJECT environment variable, if set.")
+    {
+        IsRequired = true,
+    },
+
+    new Option<string>(
+        "--clu-deployment",
+        getDefaultValue: () =>
+        {
+            return "prod";
+        },
+        description: "Conversations deployment to query.")
+    {
+        IsRequired = true,
+    },
+
+    new Option<string>(
+        "--clu-utterance",
+        getDefaultValue: () => "We'll have 2 plates of seared salmon nigiri.",
+        description: "The question to ask the Conversations service.")
+    {
+        IsRequired = true,
+    },
+
+    new Option<Uri>(
+        "--qna-endpoint",
         getDefaultValue: () =>
         {
             if (Uri.TryCreate(Environment.GetEnvironmentVariable("QUESTIONANSWERING_ENDPOINT"), UriKind.Absolute, out var endpoint))
@@ -19,45 +77,48 @@ var command = new RootCommand
 
             return null;
         },
-        description: "The Cognitive Services endpoint URI."),
+        description: "Question Answering (formerly QnA Maker) endpoint. The default is the QUESTIONANSWERING_ENDPOINT environment variable, if set.")
+    {
+        IsRequired = true,
+    },
 
     new Option<string>(
-        new[]{"-k", "--key"},
+        "--qna-key",
         getDefaultValue: () =>
         {
             return Environment.GetEnvironmentVariable("QUESTIONANSWERING_KEY");
         },
-        description: "Shared API key. The default is the QUESTIONANSWERING_KEY environment variable, if set.")
+        description: "Question Answering API key. The default is the QUESTIONANSWERING_KEY environment variable, if set.")
     {
         IsRequired = true,
     },
 
     new Option<string>(
-        new[]{"-p", "--project"},
+        "--qna-project",
         getDefaultValue: () =>
         {
             return Environment.GetEnvironmentVariable("QUESTIONANSWERING_PROJECT");
         },
-        description: "The project to query. The default is the QUESTIONANSWERING_PROJECT environment variable, if set.")
+        description: "Question Answering project to query. The default is the QUESTIONANSWERING_PROJECT environment variable, if set.")
     {
         IsRequired = true,
     },
 
     new Option<string>(
-        "--deployment",
+        "--qna-deployment",
         getDefaultValue: () =>
         {
             return "test";
         },
-        description: "The deployment to query. The default is \"test\".")
+        description: "Question Answering deployment to query.")
     {
         IsRequired = true,
     },
 
     new Option<string>(
-        "--question",
+        "--qna-question",
         getDefaultValue: () => "How long should my Surface battery last?",
-        description: "The question to ask. The default is \"How long should my Surface battery last?\".")
+        description: "The question to ask the Question Answering service.")
     {
         IsRequired = true,
     },
@@ -67,10 +128,9 @@ var command = new RootCommand
         "Enable debug logging."),
 };
 
-command.Description = "Cognitive Service - Language sample application for testing new releases.";
-command.Handler = CommandHandler.Create<Uri, string, string, string, string, bool>(async (endpoint, key, project, deployment, question, debug) =>
+command.Handler = CommandHandler.Create<Options>(async options =>
 {
-    using var _debug = debug ? new AzureEventSourceListener(
+    using var _debug = options.Debug ? new AzureEventSourceListener(
         (args, message) =>
         {
             var fg = Console.ForegroundColor;
@@ -95,12 +155,10 @@ command.Handler = CommandHandler.Create<Uri, string, string, string, string, boo
         },
         EventLevel.Verbose) : null;
 
-    var credential = new AzureKeyCredential(key);
-    var qnaClient = new QuestionAnsweringClient(endpoint, credential);
-    var qnaProject = new QuestionAnsweringProject(project, deployment);
+    var qnaClient = new QuestionAnsweringClient(options.QnaEndpoint, options.QnaCredential);
 
-    Console.WriteLine($"Asking: \x1b[32m{question}\x1b[m");
-    AnswersResult result = await qnaClient.GetAnswersAsync(question, qnaProject);
+    Console.WriteLine($"Asking \x1b[33mQuestion Answering\x1b[m: \x1b[32m{options.QnaQuestion}\x1b[m");
+    AnswersResult result = await qnaClient.GetAnswersAsync(options.QnaQuestion, options.QnaProjectModel);
 
     foreach (KnowledgeBaseAnswer answer in result.Answers)
     {
@@ -111,3 +169,25 @@ command.Handler = CommandHandler.Create<Uri, string, string, string, string, boo
 });
 
 return await command.InvokeAsync(args);
+
+class Options
+{
+    public Uri QnaEndpoint { get; set; }
+    public string QnaKey { get; set; }
+    public string QnaProject { get; set; }
+    public string QnaDeployment { get; set; }
+    public string QnaQuestion { get; set; }
+
+    public AzureKeyCredential QnaCredential => new AzureKeyCredential(QnaKey);
+    public QuestionAnsweringProject QnaProjectModel => new QuestionAnsweringProject(QnaProject, QnaDeployment);
+
+    public Uri CluEndpoint { get; set; }
+    public string CluKey { get; set; }
+    public string CluProject { get; set; }
+    public string CluDeployment { get; set; }
+    public string CluUtterance { get; set; }
+
+    public AzureKeyCredential CluCredential => new AzureKeyCredential(CluKey);
+
+    public bool Debug { get; set; }
+}
